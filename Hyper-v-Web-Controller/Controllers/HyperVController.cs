8f90407e4752619b73cf61d1e86afca834a0f8bd
prochuck <66011374+prochuck@users.noreply.github.com>
2022-07-12 16:25:09 +0300

    InitialCommit

---
+++ Hyper-v-Web-Controller/Controllers/HyperVController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Management;
+
+namespace Hyper_v_Web_Controller.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HyperVController : Controller
+    {
+        [HttpGet]
+        public string ShowVMs()
+        {
+            string result = "";
+            ManagementScope scope = new ManagementScope(@"\root\virtualization\v2", null);
+            string vmQueryWql = string.Format(CultureInfo.InvariantCulture,
+                        "SELECT * FROM {0}", "Msvm_ComputerSystem");
+
+            SelectQuery vmQuery = new SelectQuery(vmQueryWql);
+
+            using (ManagementObjectSearcher vmSearcher = new ManagementObjectSearcher(scope, vmQuery))
+            using (ManagementObjectCollection vmCollection = vmSearcher.Get())
+            {
+                if (vmCollection.Count == 0)
+                {
+                    throw new ManagementException(string.Format(CultureInfo.CurrentCulture,
+                        "No {0} could be found with name \"{1}\"",
+                        "Msvm_ComputerSystem",
+                        "korolko_mint"));
+                }
+
+                ManagementObject vm = null;
+                foreach (ManagementObject managementObject in vmCollection)
+                {
+                    result += managementObject.GetPropertyValue("ElementName");
+                }
+            }
+            return result;
+        }
+        
+    }
+}
