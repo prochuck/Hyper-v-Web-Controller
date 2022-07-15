a0736b34802394e3bfbf23f0e9b4aed06563e3e6
prochuck <66011374+prochuck@users.noreply.github.com>
2022-07-15 20:58:59 +0300

    Revert "Revert "Сделана аутентификация и авторизация пользователей""
    This reverts commit 6be11c35f084db59fb478b0ddb49687ef9426ecb.

---
+++ Hyper-v-Web-Controller/Interfaces/IVMImageRepository.cs
@@ -0,0 +1,14 @@
+﻿using Hyper_v_Web_Controller.Models;
+
+namespace Hyper_v_Web_Controller.Interfaces
+{
+    public interface IVMImageRepository
+    {
+        IEnumerable<VMImage> GetList();
+        VMImage Get(int id);
+        void Create(VMImage item);
+        void Update(VMImage item);
+        void Delete(int id);
+        void Save();
+    }
+}
--- Hyper-v-Web-Controller/Program.cs
+++ Hyper-v-Web-Controller/Program.cs
@@ -1,9 +1,30 @@
+using Hyper_v_Web_Controller;
+using Hyper_v_Web_Controller.Interfaces;
+using Hyper_v_Web_Controller.Models;
+using Hyper_v_Web_Controller.Services;
+using Microsoft.EntityFrameworkCore;
+using Hyper_v_Web_Controller.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddScoped<IHyperVThing, HyperVThing>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDBContext>(e => e.UseSqlServer(@"Data Source=(local);Database=HyperVWebController;Trusted_Connection=True;"));
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IVMImageRepository, VMImageRepository>();
+builder.Services.AddTransient<IVMRepository, VMRepository>();
 
+
+builder.Services.AddAuthentication("Cookies").AddCookie(o =>
+{
+    o.ExpireTimeSpan = TimeSpan.FromHours(1);
+    o.LoginPath = "/Authentication/Login";
+    o.LogoutPath = "/Authentication/Logout";
+});
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -16,8 +37,13 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseRouting();
 app.UseAuthorization();
+
+
+
+
 app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 
